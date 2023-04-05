d2f76714e1989d3889f634fe320c08d62c2eead1
422UR4H <tiagosindra@id.uff.br>
2023-04-05 00:06:33 -0300

    cards flip and unflip in pairs after a second

--- script.js
+++ script.js
@@ -1,18 +1,62 @@
 let amountCards;
-// do {
-//     amountCards = Number(prompt("Com quantas cartas quer jogar? (insira um valor par, de 4 a 14)"));
-// } while (amountCards < 4 || amountCards > 14 || (amountCards % 2) === 1 || isNaN(amountCards));
+do {
+    amountCards = Number(prompt("Com quantas cartas quer jogar? (insira um valor par, de 4 a 14)"));
+} while (authAmount(amountCards));
 
-const cards = [];
-for (let i = 0; i < parseInt(amountCards / 2); i ++) {
-    cards.push(`<div class="card id${i}">
-    <div class="front-face face">
-        <img src="./images/back.png" alt="carta virada para baixo">
-    </div>
-    <div class="back-face face">
-        Verso
-    </div>
-</div`);
+function authAmount(amount) {
+    return (amountCards < 4 || amountCards > 14 || (amountCards % 2) === 1 || isNaN(amountCards));
+}
+
+const cardList = [];
+for (let i = 0; i < amountCards; i++) {
+    let card = document.querySelector(".table .hidden");
+    card.classList.remove("hidden");
+    cardList.push(card.outerHTML);
+}
+
+function comparador() { 
+	return Math.random() - 0.5; 
+}
+cardList.sort(comparador);
+
+let HTMLTable = "";
+for (let i = 0; i < amountCards; i++) {
+    HTMLTable += cardList[i];
+    
+}
+
+let table = document.querySelector(".table");
+table.innerHTML = HTMLTable;
 
 
+function turnUp(card) {
+    let front = card.querySelector('.front-face');
+    front.classList.add('turn180');
+
+    let back = card.querySelector('.back-face');
+    back.classList.add('turn0');
+}
+
+function turnDown(card) {
+    let front = card.querySelector('.front-face');
+    front.classList.remove('turn180');
+
+    let back = card.querySelector('.back-face');
+    back.classList.remove('turn0');
+}
+
+
+let firstCardTurned = false;
+let firstCard;
+
+function choice(card) {
+    if (firstCardTurned) {
+        setTimeout(turnDown, 1000, card);
+        setTimeout(turnDown, 1000, firstCard);
+    } else {
+        firstCard = card;
+    }
+    turnUp(card);
+    
+    firstCardTurned = !firstCardTurned;
 }
